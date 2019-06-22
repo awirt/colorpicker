b253861354ff1492a732f356f8791f9a138b2f8c
Andrew Wirt <15827151+awirt@users.noreply.github.com>
2019-06-22 12:55:21 -0500

    Set newColor in Local Storage

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ changeColorBtn.on("click", changeColor);
 function changeColor() {
   var color = $('.color').val();
   newColor.css('backgroundColor', color);
+  localStorage.setItem("newColor", color);
 }
